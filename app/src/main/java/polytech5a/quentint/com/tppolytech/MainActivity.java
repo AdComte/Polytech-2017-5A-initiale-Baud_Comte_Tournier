06030231234e03fdc22219038ffdc5150a563330
QuentinTournier <quentin.tournier2@gmail.com>
2017-10-18 11:29:56 +0200

    End of 18/11/12 working session (broacast receiver done)

--- app/src/main/java/polytech5a/quentint/com/tppolytech/MainActivity.java
+++ app/src/main/java/polytech5a/quentint/com/tppolytech/MainActivity.java
@@ -1,12 +1,17 @@
 package polytech5a.quentint.com.tppolytech;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
 
     }
+
 
 
 }
