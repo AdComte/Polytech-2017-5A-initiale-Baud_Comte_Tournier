9535d71d90a1399270a82e188812c2af76fa9f10
QuentinTournier <quentin.tournier2@gmail.com>
2017-10-18 11:54:32 +0200

    Created beer in model

---
+++ app/src/main/java/polytech5a/quentint/com/tppolytech/models/Beer.java
@@ -0,0 +1,13 @@
+package polytech5a.quentint.com.tppolytech.models;
+
+/**
+ * Created by Quentin on 18/10/2017.
+ */
+
+public class Beer {
+    private String name;
+    private int id;
+    private String image_url;
+    private String description;
+    private float abv;
+}
